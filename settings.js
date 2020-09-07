cad6c06a6ff31368ee22b9ceecb51541cb5d1a75
sreekanthp02 <47383220+sreekanthp02@users.noreply.github.com>
2020-09-07 08:42:11 +0000

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,11 @@
+﻿exports.dbConfig = {
+    user: "postman",
+    password: "sqlserver123$",
+    server: "postman.database.windows.net", 
+    database: "postman",
+    port: 1433
+};
+
+exports.webPort = 8000;
+
+exports.httpMsgsFormat = "JSON";
